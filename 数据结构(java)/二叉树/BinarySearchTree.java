36138efb343b17f4deca8ec566547bf3461518e4
songshaoming <ssm_x@foxmail.com>
2017-12-26 18:28:16 +0800

    update 2017年12月26日18点28分

---
+++ 数据结构(java)/二叉树/BinarySearchTree.java
@@ -0,0 +1,83 @@
+package 二叉树;
+
+/**
+ * 二叉搜索树
+ * 
+ * @author ssm
+ *
+ */
+public class BinarySearchTree
+{
+	/**
+	 * 查找操作 递归算法
+	 * 
+	 * @param x
+	 * @param root
+	 * @return
+	 */
+	TreeNode find(int x, TreeNode root)
+	{
+		if (root == null)// 树为空，返回NULL
+			return null;
+
+		if (x < root.data)// X小于根结点键值，只需在左子树中继续搜索
+			return find(x, root.left);
+		else if (x > root.data)// X大于根结点的键值， 在右子树中进行继续搜索
+			return find(x, root.right);
+		else
+			return root;// 查找成功， 返回结点的找到结点的地址
+	}
+
+	/**
+	 * 查找操作 非递归算法
+	 * 
+	 * @param x
+	 * @param root
+	 * @return
+	 */
+	TreeNode find2(int x, TreeNode root)
+	{
+		while (root != null)
+		{
+			if (x < root.data)
+				root = root.left;// 向左子树中移动， 继续查找
+			else if (x > root.data)
+				root = root.right;// 向右子树中移动， 继续查找
+			else
+				return root;// 查找成功， 返回结点的找到结点的地址
+		}
+		return null;// 查找失败
+	}
+
+	/**
+	 * 查找最小元素 递归函数
+	 * 
+	 * @param root
+	 * @return
+	 */
+	TreeNode findMin(TreeNode root)
+	{
+		if (root == null)// 空的二叉搜索树，返回NULL
+			return null;
+		else if (root.left == null)
+			return root;// 找到最左叶结点并返回
+		else
+			return findMin(root.left);// 沿左分支继续查找
+	}
+
+	/**
+	 * 查找最大元素 非递归函数
+	 * 
+	 * @param root
+	 * @return
+	 */
+	TreeNode findMax(TreeNode root)
+	{
+		if (root != null)
+		{
+			while (root.right != null)
+				root = root.right;
+		}
+		return root;
+	}
+}
